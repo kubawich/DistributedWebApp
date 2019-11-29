a05ed60a940249b5bc36458c07cf650088aac38b
Kuba Wichliński <kubawich45@gmail.com>
2019-11-29 18:43:26 +0100

    Initial commit

---
+++ DistributedWebApp/DistributedWebAppBenchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DistributedWebAppBenchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
