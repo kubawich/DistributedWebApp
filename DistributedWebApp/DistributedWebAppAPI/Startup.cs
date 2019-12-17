63fb9d9419c47df4c9d0ce64ea73267448e18715
Maciej Wawrzyniak <maciej.wawrzyniakbr@gmail.com>
2019-12-17 19:52:30 +0100

    Oczyszczenie kodu ze zbędnych elementów

--- DistributedWebApp/DistributedWebAppAPI/Startup.cs
+++ DistributedWebApp/DistributedWebAppAPI/Startup.cs
@@ -1,17 +1,11 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace DistributedWebAppAPI
@@ -25,12 +19,9 @@ namespace DistributedWebAppAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
-            // Register the Swagger generator, defining 1 or more Swagger documents
             ConfigureSwagger(services);
         }
 
@@ -63,11 +54,9 @@ namespace DistributedWebAppAPI
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseSwagger();
-
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
@@ -79,11 +68,8 @@ namespace DistributedWebAppAPI
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
